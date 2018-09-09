62dd82192fe461e212272c1bc09180f83d89ff13
Jn_xyp <jinan.xyp@gmail.com>
2018-09-08 20:06:20 -0400

    Add link to the calendar page on newtab.html

--- calendar_init.js
+++ calendar_init.js
@@ -1,1 +1,27 @@
 'use strict';
+$(function () {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'month, list, agenda',
+            center: 'title',
+            right: 'today, prev, next'
+        },
+        themeSystem: 'bootstrap4',
+        buttonText: {
+            prev: '< Previous',
+            next: 'Next >',
+            today: 'Today',
+            month: 'Month',
+            list: 'List',
+            agenda: 'Agenda'
+        },
+        selectable:'true',
+        nowIndicator:'true'
+    })
+
+});
+
+var calendar = $('#calendar').fullCalendar('getCalendar');
